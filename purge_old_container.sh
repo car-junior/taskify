afe144b7b7a300aa7d53756751aeda6d7683de4f
car-junior <carlosilva.dev@gmail.com>
2024-03-06 03:35:05 -0300

    feat(gitlab-ci): Ajusta script para purge de old containers, criando logica para excluir/parar apenas quando existe

--- purge_old_container.sh
+++ purge_old_container.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-docker stop taskify 2>/dev/null
-docker rm taskify 2>/dev/null
+container_name="taskify"
+
+# Verifica se existe alguma container com o nome informado
+if docker inspect "$container_name" &> /dev/null; then
+
+    # Verifica se existe um container em execução com o nome informado
+    if docker ps -f name="$container_name" --format '{{.Names}}' | grep -q "$container_name"; then
+      echo "Parando container $container_name..."
+      docker stop "$container_name"
+      echo "Container parado com sucesso!"
+    fi
+
+    echo "Removendo container $container_name..."
+    docker rm "$container_name"
+    echo "Container removido com sucesso!"
+fi
